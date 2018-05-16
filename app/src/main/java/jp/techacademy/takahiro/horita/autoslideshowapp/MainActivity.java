17eb910b85e455f7626cad24b60b5e47c7dde915
TakahiroHorita <37589728+TakahiroHorita@users.noreply.github.com>
2018-05-16 17:28:58 +0900

    5/16 17:30

--- app/src/main/java/jp/techacademy/takahiro/horita/autoslideshowapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/takahiro/horita/autoslideshowapp/MainActivity.java
@@ -7,35 +7,28 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
-import android.provider.MediaStore;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int PERMISSIONS_REQUEST_CODE = 100;
-
+    ArrayList<Uri> arrayList = new ArrayList<>();
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button forwardButton = findViewById(R.id.forward_button);
-        forwardButton.setOnClickListener(new forwardButtonClickListener());
-
-        Button buckButton = findViewById(R.id.buck_button);
-        buckButton.setOnClickListener(new buckButtonClickListener());
-
-        Button playstopButton = findViewById(R.id.playstop_button);
-        playstopButton.setOnClickListener(new playstopButtonClickListener());
-
-
+        // Android 6.0以降の場合
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             // パーミッションの許可状態を確認する
             if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
@@ -49,6 +42,45 @@ public class MainActivity extends AppCompatActivity {
         } else {
             getContentsInfo();
         }
+
+        Button forward_button = findViewById(R.id.forward_button);
+        forward_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ImageView imageView = findViewById(R.id.imageView);
+                if(arrayList.size()-1 > i){
+                    i++;
+                    imageView.setImageURI(arrayList.get(i));
+                }else{
+                    i = 0;
+                    imageView.setImageURI(arrayList.get(i));
+                }
+            }
+        });
+
+        Button buck_button = findViewById(R.id.buck_button);
+        buck_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView imageView = findViewById(R.id.imageView);
+                if(i > 0){
+                    i--;
+                    imageView.setImageURI(arrayList.get(i));
+                }else{
+                    i=arrayList.size()-1;
+                    imageView.setImageURI(arrayList.get(i));
+                }
+            }
+        });
+
+        Button playstop_button = findViewById(R.id.playstop_button);
+        playstop_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
@@ -65,9 +97,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getContentsInfo() {
-
         // 画像の情報を取得する
         ContentResolver resolver = getContentResolver();
+
         Cursor cursor = resolver.query(
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
                 null, // 項目(null = 全項目)
@@ -77,39 +109,21 @@ public class MainActivity extends AppCompatActivity {
         );
 
         if (cursor.moveToFirst()) {
-            // indexからIDを取得し、そのIDから画像のURIを取得する
-            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-            Long id = cursor.getLong(fieldIndex);
-            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
 
-            ImageView imageView = findViewById(R.id.imageView);
-            imageView.setImageURI(imageUri);
+            do {
+                // indexからIDを取得し、そのIDから画像のURIを取得する
+                int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                Long id = cursor.getLong(fieldIndex);
+                Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+                arrayList.add(imageUri);
+
+                ImageView imageView = findViewById(R.id.imageView);
+                imageView.setImageURI(arrayList.get(0));
+
+            } while (cursor.moveToNext());
+
         }
         cursor.close();
     }
-
-    class forwardButtonClickListener implements View.OnClickListener{
-
-        @Override
-        public void onClick(View v) {
-
-        }
-    }
-
-    class buckButtonClickListener implements View.OnClickListener{
-
-        @Override
-        public void onClick(View v) {
-
-        }
-    }
-
-    class playstopButtonClickListener implements View.OnClickListener{
-
-        @Override
-        public void onClick(View v) {
-
-        }
-    }
-
 }
